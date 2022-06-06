a309531010ca30b6e26579e2b2321086b0aa4fe0
saibot777 <stefantrajkovic777@gmail.com>
2022-06-06 12:31:23 +0200

    Update UI

--- src/App/UnderBar/style.ts
+++ src/App/UnderBar/style.ts
@@ -1,7 +1,3 @@
-/* Creative Commons Attribution 4.0 International (CC-BY-4.0) */
-/* Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com) */
-/* This source code was getting from https://github.com/tastejs/todomvc-app-css/blob/03e753aa21bd555cbdc2aa09185ecb9905d1bf16/index.css */
-
 import styled from 'styled-components'
 
 export const Layout = styled.div`
